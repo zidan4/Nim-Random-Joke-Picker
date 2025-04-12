468701d806db05f2551bfadc2e33c46c761a010f
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2025-04-12 16:10:09 +0300

    Create jokes.nim

---
+++ jokes.nim
@@ -0,0 +1,7 @@
+import random
+
+let jokes = ["Why do programmers prefer dark mode? Because light attracts bugs.",
+             "There are only 10 types of people: those who understand binary and those who don’t.",
+             "I would tell you a UDP joke, but you might not get it."]
+
+echo jokes[rand(jokes.len - 1)]
